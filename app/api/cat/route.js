0aa2eae1920b11f9e6546935a05ce446c65db38c
Renan Janz H Vieira <janzhv.it@gmail.com>
2024-06-06 18:49:06 +0100

    C:/Program Files/Git/api/cat find cat with query ID implemented => Marisa/Janz

--- app/api/cat/route.js
+++ app/api/cat/route.js
@@ -1,20 +1,28 @@
 import { connectMongoDB } from "@/lib/mongodb";
-
+import mongoose from "mongoose";
+import Cat from "@/models/cat"; // Import the Cat model
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
     try {
         await connectMongoDB();
-    
+
         const url = new URL(req.url, "http://localhost");
         const params = new URLSearchParams(url.search);
         const catId = params.get("catId");
+
+        // Check if catId is valid
+        if (!mongoose.Types.ObjectId.isValid(catId)) {
+            return NextResponse.json({ message: "Invalid catId." }, { status: 400 });
+        }
+
         const getCat = await Cat.findOne({ _id: catId });
-    
+
         if (!getCat) {
             return NextResponse.json({ message: "Cat not found." }, { status: 404 });
         }
-    
-        return NextResponse.json({ user: getCat }, { status: 200 });
+
+        return NextResponse.json(getCat, { status: 200 }); // Return the cat object directly
     } catch (error) {
         console.error(error);
         return NextResponse.json(
@@ -22,4 +30,4 @@ export async function GET(req) {
             { status: 500 }
         );
     }
-    }
+}
